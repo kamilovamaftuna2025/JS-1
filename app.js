7d74d698f52b66a46815b4878ff6f210cabc63d3
Maftuna <kamilovamaftuna2025>
2025-04-05 11:35:49 +0500

    js

---
+++ app.js
@@ -0,0 +1,54 @@
+// // alert("salom")
+// const javob = confirm("Buyruqlarni bajarishga rozimisiz?")
+
+// // console.log(javob)
+
+// if (javob == true) {
+//     console.log("Rozi bo`ldi")
+// }
+//  else if(javob == false){
+//     console.log("Bekor qilindi!")
+// }
+
+// const baho = prompt("Iltimos bizni baholang!")
+
+// if(baho == 5){
+//     console.log("A`lo")
+// }
+// else if(baho == 4){
+//     console.log("Yaxshi")
+// }
+// else if(baho == 3){
+//     console.log("Qoniqarli")
+// }
+// else if(baho == 2){
+//     console.log("Yomon")
+// }
+// else if(baho == 1){
+//     console.log("Juda yomon")
+// }
+// else{
+//     console.log("iltimos,qaytadan urinip ko`ring!")
+// }
+
+let soat = prompt("Soat necha bo`ldi?")
+
+if (soat >=0 && soat < 6){
+    console.log("Good night!")
+}
+
+else if(soat >= 6 && soat < 12){
+    console.log("Good morning!")
+}
+
+else if(soat >= 12 && soat < 18){
+    console.log("Good afternoon!")
+}
+
+else if(soat >= 18 && soat < 24){
+    console.log("Good evening!")
+}
+
+else{
+    console.log("Siz noto`gri malumot kiritdingiz!")
+}
